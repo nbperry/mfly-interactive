d6eb67eb90a71eeaa2714bdf48daeb341fea84fb
Nachiket Mehta <nmehta@mediafly.com>
2017-02-28 08:52:22 -0800

    e2e tests for the cli

--- test/app/bower_components/mfly-commands/README.md
+++
@@ -1,4 +0,0 @@
-# mflyCommands
-mflyCommands.js for Mediafly Interactives
-
-API documentation can be found at http://devdocs.mediafly.com/interactives/#
---
+++ test/script.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+var exec = require('child_process').exec
+
+function canaryTest() {
+	assert.equal(true, true, 'Canary test should pass')
+}
+
+function versionTest() {
+	exec('node bin/index version', function(error, stdout, stderr) {
+		if (error) {
+			assert.fail(error, null, 'error getting version' + stderr)
+		}
+		assert(new RegExp(/Version: \d+.\d+.\d+/).test(stdout), stdout + ' is invalid')
+	});	
+}
+
+canaryTest()
+versionTest()
