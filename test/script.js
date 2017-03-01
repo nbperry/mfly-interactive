794f58a00105fb5e8fd9421ac229203f2093ac53
Nachiket Mehta <nmehta@mediafly.com>
2017-03-01 11:37:21 -0800

    add tests for version release and publish commands

--- test/script.js
+++ test/script.js
@@ -1,18 +1,77 @@
+var _ = require('lodash')
 var assert = require('assert')
+var request = require('request')
 var exec = require('child_process').exec
+var existsSync = require('fs').existsSync
+var readFileSync = require('fs').readFileSync
+var writeFileSync = require('fs').writeFileSync
+var config = require('./app/mfly-interactive.config')
+var getItemAssetState = require('../lib/item').getItemAssetState
+var uuid = require('uuid/v1')
+var uniqueId = uuid()
 
-function canaryTest() {
-	assert.equal(true, true, 'Canary test should pass')
-}
+describe('mfly-interactive', () => {
+	it('Canary test should pass', () => {
+		assert.equal(true, true, 'Canary test should pass')
+	})
 
-function versionTest() {
-	exec('node bin/index version', (error, stdout, stderr) => {
-		if (error) {
-			assert.fail(error, null, 'error getting version' + stderr)
+	it('Should display version from version command', done => {
+		exec('node bin/index version', (err, stdout) => {
+			assert(new RegExp(/Version: \d+.\d+.\d+/).test(stdout), stdout + ' is invalid')
+			done(err)
+		});	
+	})
+
+	it('Should produce .interactive file from release command', done => {
+		exec('rm test/app/app.interactive', () => {
+			exec('node ../../bin/index release', { cwd: 'test/app' }, err => {
+				assert(existsSync('test/app/app.interactive'), 'app.interactive was not created by the release command')
+				done(err)
+			})
+		})
+	})
+
+	it('Should publish an interactive', function(done) {
+		this.timeout(5 * 60 * 1000)
+		function transformHtml() {
+			var html = readFileSync('test/app/index.test.html', { encoding: 'ascii' })
+			var transformedHtml =_.template(html)({ timestamp: uniqueId })
+			writeFileSync('test/app/index.html', transformedHtml, { encoding: 'ascii' })
 		}
-		assert(new RegExp(/Version \d+.\d+.\d+/).test(stdout), stdout + ' is invalid')
-	});	
-}
 
-canaryTest()
-versionTest()
+		function waitUntilAssetStateIsProcessed(cb) {
+			var timer = setInterval(() => {
+				getItemAssetState(config.itemId, config.productId, config.accessToken, assetState => {
+					if (assetState === 'processed') {
+						clearInterval(timer)
+						cb()
+					}
+				})
+			}, 7000)
+		}
+
+		function verifyInteractivePublished(cb) {
+			request('https://d3ckn119ow13s4.cloudfront.net/' + config.mcode + '/' + config.slug + '/index.html', (err, response, body) => {
+				if (!err && response.statusCode === 200) {
+					assert(body.indexOf('<div class="testing">' + uniqueId + '</div>') > 0)
+				}
+				cb(err)
+			})
+		}
+			
+		transformHtml()
+		exec('rm test/app/app.interactive', () => {
+			exec('node ../../bin/index publish', { cwd: 'test/app' }, () => {
+				//verify interactive was published
+				waitUntilAssetStateIsProcessed(() => {
+					//now verify the change to index.html was actually extracted to S3
+					verifyInteractivePublished(done)
+				})
+			})
+		})
+	})
+})
+
+
+// function publishTest() {
+// }
