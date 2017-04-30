602a8390822f4b37d317ae0b56edce2d061b8719
Nachiket Mehta <nmehta@mediafly.com>
2017-04-30 12:17:35 -0700

    update tests to use a node specific config

--- test/script.js
+++ test/script.js
@@ -1,5 +1,4 @@
 var _ = require('lodash')
-var fs = require('fs')
 var assert = require('assert')
 var request = require('request')
 var exec = require('child_process').exec
@@ -10,17 +9,10 @@ var getItemAssetState = require('../lib/item').getItemAssetState
 var uuid = require('uuid/v1')
 var uniqueId = uuid()
 
-var baseConfig = {
-    "filename": "app.interactive",
-    "accessToken": "63b26f29837744e6adb23fce54180659",
-    "mcode": "interactives",
-    "productId": "9cf282320e6340ee8b830e5376d54531"
-}
-
 describe('mfly-interactive', function() {
 	var config
 	before(function() {
-		//modify mfly-interactive.config.json
+		//require config based on node version
 		config = require('./app/mfly-interactive-node' + process.version[1] + '.config')
 	})
 	
@@ -36,12 +28,12 @@ describe('mfly-interactive', function() {
 		exec('node bin/index version', (err, stdout) => {
 			assert(new RegExp(/Version: \d+.\d+.\d+/).test(stdout), stdout + ' is invalid')
 			done(err)
-		});	
+		})	
 	})
 
 	it('Should produce .interactive file from release command', done => {
 		exec('rm test/app/app.interactive', () => {
-			exec('node ../../bin/index release', { cwd: 'test/app' }, err => {
+			exec('node ../../bin/index release --config mfly-interactive-node' + process.version[1] + '.config.json', { cwd: 'test/app' }, err => {
 				assert(existsSync('test/app/app.interactive'), 'app.interactive was not created by the release command')
 				done(err)
 			})
